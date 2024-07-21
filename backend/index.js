c8adddc72596642f662bd620b0a5fd84359c3561
sirserg <sirserg05@gmail.com>
2024-07-21 12:16:17 +0300

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+});
