9011ba6fc4b50699fdcf9e7aa681ccc58fb4dbc7
sirserg <sirserg05@gmail.com>
2024-07-23 21:28:47 +0300

    * fix: fix sign-up not logging in bug

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -13,7 +13,8 @@ const signUp = async (req: Request, res: Response) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({ name:fullName, email, password: hashedPassword });
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET as string, { expiresIn: '30d' });
+    res.status(201).json({ message: 'User created successfully', token, user: newUser });
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Error creating user', error });
--- frontend/src/components/app/components/protectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/app/components/protectedRoute/ProtectedRoute.tsx
@@ -14,7 +14,6 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   user,
   authChecked,
 }) => {
-    console.log(user)
   if (!authChecked) {
     return <Loader />;
   }
