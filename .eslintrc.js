0c4d9302d07a14176b2c52692fbd4c0b9697aaf6
s1rserg <sirserg05@gmail.com>
2025-07-02 19:31:08 +0300

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        // Paths to all subproject tsconfigs
+        project: ['./client/tsconfig.json', './server/tsconfig.json'],
+      },
+    },
+  },
+  rules: {
+    'import/no-cycle': 'warn',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-self-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': 'warn',
+    'import/no-relative-packages': 'error',
+  },
+  overrides: [
+    {
+      files: ['client/**/*.ts', 'client/**/*.tsx'],
+      parserOptions: {
+        project: './client/tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    {
+      files: ['server/**/*.ts', 'server/**/*.tsx'],
+      parserOptions: {
+        project: './server/tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+};
